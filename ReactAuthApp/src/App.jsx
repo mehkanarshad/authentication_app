93e61242bfa1778f788c533dbfcd666137831740
mehkanarshad <mehkanarshadskp@gmail.com>
2024-10-07 12:56:50 +0500

    frontend foundation made

---
+++ ReactAuthApp/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+
+function App() {
+
+  return (
+    <>
+     <Router>
+        
+     </Router>
+    </>
+  )
+}
+
+export default App
